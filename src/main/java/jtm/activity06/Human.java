7715d12b28e65361e94f4e3b3a0fed5fe80397ef
'Sintija-Git' <'sintija.jaspere@gmail.com'>
2020-05-22 21:58:37 +0300

    adding activity06 / Interfaces

---
+++ src/main/java/jtm/activity06/Human.java
@@ -0,0 +1,92 @@
+package jtm.activity06;
+
+
+public class Human implements Humanoid {
+
+	private int stomach = 0;
+	private int weight = BirthWeight;
+	private int vomit = 0;
+	private boolean isAlive = true;
+
+	public Human() {} //default constructor
+	
+	
+	@Override
+	public void eat(Integer food) {
+	
+		if(stomach == 0) {
+			stomach += food;
+			weight += food;
+		}
+	}
+	
+
+	@Override
+	public Integer vomit() {
+		vomit = stomach;
+		if(stomach != 0) {
+		weight -= stomach;
+		stomach = 0;
+		}
+		return vomit;
+	}
+
+	
+	@Override
+	public String isAlive() { 
+		if(isAlive == true) {
+		return "Alive";
+		} else {
+		return "Dead";
+		}			
+	}
+	
+	
+	@Override
+	public String killHimself() {
+		isAlive = false;
+		return "Dead";	
+	}
+
+	
+ 
+	@Override
+	public int getWeight() {
+		weight = Humanoid.BirthWeight + stomach;
+		return weight;	
+	}
+	
+	
+	@Override
+	public String toString() {
+		return getClass().getSimpleName() + ": " + getWeight()  +" ["+ stomach +"]";
+	}
+	
+	
+	//getters&setters
+
+	public int getStomach() {
+		return stomach;
+	}
+
+	public void setStomach(int stomach) {
+		this.stomach = stomach;
+	}
+
+	public int getVomit() {
+		return vomit;
+	}
+
+	public void setVomit(int vomit) {
+		this.vomit = vomit;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public void setAlive(boolean isAlive) {
+		this.isAlive = isAlive;
+	}
+	
+} //end class
---
+++ src/main/java/jtm/activity06/Martian.java
@@ -0,0 +1,103 @@
+package jtm.activity06;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Martian implements Humanoid, Alien, Cloneable {
+	
+	private int weight = 0;
+	Object vomit = null;
+	ArrayList<Object> contenOfStomach = new ArrayList<Object> ();
+	
+	//SETTERS GETTERS FOR VARIABLES
+	
+	
+	
+	@Override
+    public Object clone() throws CloneNotSupportedException {
+    return clone(this);
+	}
+	
+//  TODO implement cloning of current object
+//  and its stomach
+	private Object clone(Object current) throws CloneNotSupportedException { 
+	return super.clone();
+
+	}
+	
+
+	
+	/**
+	 * @param If Alien is hungry (stomach is empty), eat Object. Possibly eaten
+	 *           items are:
+	 * 
+	 *           1. Integer, 2. Humanoid, 3. Alien.
+	 * 
+	 *           Gain weight of eaten item, and kill it, if possible
+	 */
+	
+	
+	@Override
+	public void eat(Integer food) {
+		if (food instanceof Integer) {
+			contenOfStomach.add(food);
+			weight = getWeight();
+		}
+		
+	}
+	@Override
+	public void eat(Object item) {
+		
+		if (item instanceof Human) {
+		weight = ((Human) item).getWeight();
+		 contenOfStomach.add(item);
+		 ((Human) item).killHimself();
+		
+		} else if (item instanceof Alien) {
+		weight = ((Martian) item).getWeight();
+		contenOfStomach.add(item);	
+		}
+	}
+
+	
+	@Override
+	public Object vomit() {
+
+		if (contenOfStomach.equals(null)) {
+			 return null;
+		} else {
+			vomit = contenOfStomach;
+			contenOfStomach.clear();
+	}
+		return vomit;
+	}
+
+	@Override
+	public String isAlive() {
+		
+		return "I AM IMMORTAL!";
+	}
+
+	@Override
+	public String killHimself() {
+		
+		return "I AM IMMORTAL!";
+	}
+
+	@Override
+	public int getWeight() {
+		weight = Alien.BirthWeight + weight;
+		return weight;
+	}
+	
+	
+	
+	@Override
+	public String toString() {
+		
+		return getClass().getSimpleName() + ": " + weight + " " + contenOfStomach; 
+	}
+	
+	
+
+}
