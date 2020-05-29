8c67bdc309b2eb789f4f2caa2e6ce296a17b4724
'Sintija-Git' <'sintija.jaspere@gmail.com'>
2020-05-29 16:09:03 +0300

    Added activity08 / Exceptions

---
+++ src/main/java/jtm/activity08/SimpleCalcException.java
@@ -0,0 +1,23 @@
+package jtm.activity08;
+
+public class SimpleCalcException extends Exception {
+
+
+
+	public SimpleCalcException(String message) {
+		super(message);
+		
+	}
+
+
+	public SimpleCalcException(String message, Throwable cause) {
+		super(message, cause);
+	
+	}
+
+//	public SimpleCalcException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+//		super(message, cause, enableSuppression, writableStackTrace);
+//		// TODO Auto-generated constructor stub
+//	}
+
+}
