fc6f21eea68f98355358d785b39e2d3fac7e4387
'Sintija-Git' <'sintija.jaspere@gmail.com'>
2020-06-02 18:46:51 +0300

    Adding activity09 / Collections

--- src/main/java/jtm/activity09/Order.java
+++ src/main/java/jtm/activity09/Order.java
@@ -25,9 +25,88 @@ package jtm.activity09;
  * 
  */
 
-public class Order {
+public class Order implements Comparable<Order> {
 	String customer; // Name of the customer
 	String name; // Name of the requested item
 	int count; // Count of the requested items
+	
 
-}
+	// public Order(String orderer, String itemName, Integer count) — constructor of
+	// the Order
+	public Order(String orderer, String itemName, Integer count) {
+		customer = orderer;
+		name = itemName;
+		this.count = count;
+	}
+
+	
+	// public int compareTo(Order order) — comparison implementation according to
+	// logic described below
+	@Override
+	public int compareTo(Order object) {
+
+		int result = this.name.compareTo(object.name);
+		if (result > 0) {
+			return 1;
+		} else if (result < 0) {
+			return -1;
+		} else if (result == 0) {
+			result = this.customer.compareTo(object.customer);
+
+			if (result > 0) {
+				return 1;
+			} else if (result < 0) {
+				return -1;
+			} else if (result == 0) {
+				result = Integer.compare(this.count, object.count);
+			}
+		}
+		return result;
+	}
+
+	
+	@Override
+	public boolean equals(Object object) { // — check equality of orders
+
+		if (object instanceof Order) {
+
+			Order order = (Order) object;
+			return this.name.equals(order.name) && this.customer.equals(order.customer) && this.count == order.count;
+		} else {
+			return false;
+		}
+	}
+	
+
+	@Override
+	public int hashCode() { // — to be able to handle it in some hash...collection
+
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + count;
+		result = prime * result + ((customer == null) ? 0 : customer.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public String toString() { // — string in following form: "ItemName: OrdererName: Count"
+
+		return name + ": " + customer + ": " + count;
+	}
+
+	
+//	// getters
+//	public String getCustomer() {
+//		return customer;
+//	}
+//
+//	public String getItemName() {
+//		return name;
+//	}
+//
+//	public int getCount() {
+//		return count;
+//	}
+
+} // end class
--- src/main/java/jtm/activity09/Orders.java
+++ src/main/java/jtm/activity09/Orders.java
@@ -1,12 +1,22 @@
 package jtm.activity09;
 
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
+import java.util.Set;
 
 /*- TODO #2
  * Implement Iterator interface with Orders class
  * Hint! Use generic type argument of iterateable items in form: Iterator<Order>
  * 
  * TODO #3 Override/implement public methods for Orders class:
- * - Orders()                — create new empty Orders
+ * - Orders()               	 — create new empty Orders
  * - add(Order item)            — add passed order to the Orders
  * - List<Order> getItemsList() — List of all customer orders
  * - Set<Order> getItemsSet()   — calculated Set of Orders from list (look at description below)
@@ -29,7 +39,12 @@ package jtm.activity09;
  *  - ItemN: Customer1,Customer2: 4
  */
 
-public class Orders {
+public class Orders implements Iterator<Order> {
+
+	String customer; // Name of the customer
+	String name; // Name of the requested item
+	int count; // Count of the requested items
+
 	/*-
 	 * TODO #1
 	 * Create data structure to hold:
@@ -40,4 +55,84 @@ public class Orders {
 	 *   2. when constructing list of orders, set number of current order to -1
 	 *      (which is usual approach when working with iterateable collections).
 	 */
-}
+
+	List<Order> ordersList = new LinkedList<Order>();
+	ListIterator<Order> itr = ordersList.listIterator();
+
+
+	public Orders() {
+		ordersList = new LinkedList<Order>();
+	}
+
+	public void add(Order item) { // — add passed order to the Orders
+		ordersList.add(item);
+
+	}
+
+	public List<Order> getItemsList() { // — List of all customer orders
+		return ordersList;
+
+	}
+
+	public void sort() { // — Use built in List.sort() method to sort list of orders
+		Collections.sort(ordersList);
+	}
+
+	/*
+	 * When implementing getItemsSet() method, join all requests for the same item
+	 * from different customers in following way: if there are two requests: -
+	 * ItemN: Customer1: 3 - ItemN: Customer2: 1 Set of orders should be: - ItemN:
+	 * Customer1,Customer2: 4
+	 */
+	public Set<Order> getItemsSet() { // — calculated Set of Orders from list (look at description below)
+		
+		Set<Order> set = new HashSet<>();
+
+		for (int i = 0; i < ordersList.size(); i++) {
+			Order current = ordersList.get(i);
+			if (!set.contains(current)) {
+				set.add(current);
+				System.out.println(set);
+			}
+
+		}
+		return set;
+	}
+
+	@Override
+	public boolean hasNext() { // — check is there next Order in Orders
+		if (itr.hasNext()) {
+			return true;
+		}
+		return false;
+	}
+	
+
+	@Override
+	public Order next() { // — get next Order from Orders, throw NoSuchElementException if can't
+
+		if (itr.hasNext()) {
+			return itr.next();
+		} else {
+			throw new NoSuchElementException();
+		}
+	}
+
+	public void remove() { // — remove current Order (order got by previous next()) from list, throw
+							// IllegalStateException if can't
+
+		if (itr.hasNext()) {
+			itr.remove();
+		} else {
+			throw new IllegalStateException();
+
+		}
+	}
+
+	@Override
+	public String toString() { // — show list of Orders as a String
+		return ordersList.toString();
+
+	}
+
+} // end class
