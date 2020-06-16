91e90b77459caa08813aaaab15007ba10b739fb5
'Sintija-Git' <'sintija.jaspere@gmail.com'>
2020-06-16 21:56:51 +0300

    extra06/Generics

--- src/main/java/jtm/extra06/Generics.java
+++ src/main/java/jtm/extra06/Generics.java
@@ -1,5 +1,6 @@
 package jtm.extra06;
 
+import static jtm.extra06.GenericsTest.log;
 import java.util.LinkedList;
 // TODO #1
 // import statically jtm.extra06.GenericsTest.log StringBuilder object
@@ -7,19 +8,38 @@ import java.util.LinkedList;
 // Note that necessary messages should be written there
 
 public class Generics<E extends Number> extends LinkedList<E> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7708818085602746688L;
 	// TODO #2
 	// Use Eclipse code generation prompter to add generated serial version ID
 	// for this class to avoid warning
+
+	
+	
 
 	// TODO #3
 	// Select Source— Generate Constructors from Superclass... and select
 	// LinkedList<E>().
 	// And implement extended constructor that after new Generics object is
 	// created, log has appended line "Generics instance created"
+	
+	public Generics() {
+		super();
+		log.append("Generics instance created");
+		
+	}
+
 
 	// TODO #4
 	// Select menu: Source— Override/Implement methods..., extend LinkedList<E>
 	// class, and select push(E) and pop() methods to override them.
+	
+
+
+	
 	// TODO #4.1
 	// Override pop() method of the list, that besides popping element from it
 	// log has appended line: "java.lang.Integer: 1 popped", where:
@@ -27,9 +47,29 @@ public class Generics<E extends Number> extends LinkedList<E> {
 	// HINT:
 	// You can use this.peek() method to refer to any object being popped from
 	// list
+	
+
+	
 	// TODO #4.2
 	// override push() method that besides pushing new element into list
 	// log has appended line: "java.lang.Double: 2 pushed", where:
 	// java.lang.Double is actual class name of the value
 	// 2 is its actual value
-}
+	
+	@Override
+	public void push(E e) {
+		super.push(e);
+		log.append("\n" + this.peek().getClass().getName() + ": " + this.peek() + " pushed");
+		
+	}
+	
+	@Override
+	public E pop() {
+		log.append("\n" +  this.peek().getClass().getName() +  ": " + this.peek() + " popped");
+		return super.pop();
+		
+		
+	}
+	
+
+} //end class
